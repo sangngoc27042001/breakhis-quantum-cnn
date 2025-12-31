7d51dae557719f83c6ebe9e41621232e6b7adea4
sangvorennlabs <sang.vo@rennlabs.com>
2025-12-31 08:10:18 +0200

    ok

--- src/config.py
+++ src/config.py
@@ -51,7 +51,7 @@ CLASS_WEIGHTS = {
 # ============================================================================
 # TRAINING HYPERPARAMETERS
 # ============================================================================
-BATCH_SIZE = 256
+BATCH_SIZE = 128
 EPOCHS = 20
 INITIAL_LEARNING_RATE = 1e-3
 MIN_LEARNING_RATE = 1e-6
